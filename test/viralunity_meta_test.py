2e252da4c9c9d38385846250b3ecfea34e7c690c
Felippe Nacif <felippenacif@gmail.com>
2025-01-25 12:30:52 -0300

    test: add unit tests for argument validation and config file generation

--- test/viralunity_meta_test.py
+++ test/viralunity_meta_test.py
@@ -1,40 +1,17 @@
 import unittest
 from unittest.mock import patch
-from viralunity.viralunity_meta import get_args, validate_args
+from viralunity.viralunity_meta import (
+    get_args,
+    validate_args,
+    generate_config_file,
+    main,
+)
 import os
 
 print(os.environ.get("PATH"))
 
 
 class Test_RequiredArgs(unittest.TestCase):
-    all_args = [
-        "--data-type",
-        "illumina",
-        "--sample-sheet",
-        "sample_sheet.csv",
-        "--config-file",
-        "config_file.yaml",
-        "--output",
-        "output_dir",
-        "--run-name",
-        "run_name",
-        "--kraken2-database",
-        "kraken2_db",
-        "--krona-database",
-        "krona_db",
-        "--remove-human-reads",
-        "--remove-unclassified-reads",
-        "--adapters",
-        "adapters.fasta",
-        "--minimum-read-length",
-        "50",
-        "--trim",
-        "0",
-        "--create-config-only",
-        "--threads",
-        "1",
-    ]
-
     def test_required_args_fail_when_only_optional_set(self):
         args = [
             "--run-name",
@@ -52,7 +29,7 @@ class Test_RequiredArgs(unittest.TestCase):
             "1",
         ]
         with self.assertRaises(SystemExit):
-                get_args(args)
+            get_args(args)
 
     def test_required_args_success_when_only_required_set(self):
         args = [
@@ -160,23 +137,188 @@ class Test_ValidateArgs(unittest.TestCase):
     @patch("os.path.isfile", side_effect=[True, False])
     @patch("os.path.isdir", side_effect=[False, False])
     @patch("viralunity.viralunity_meta.validate_sample_sheet", return_value={})
-    def test_validate_args_kraken2_db_not_exist(self, mock_validate_sample_sheet, mock_isdir, mock_isfile):
+    def test_validate_args_kraken2_db_not_exist(
+        self, mock_validate_sample_sheet, mock_isdir, mock_isfile
+    ):
         with self.assertRaises(SystemExit):
             validate_args(self.args)
 
     @patch("os.path.isfile", side_effect=[True, False])
     @patch("os.path.isdir", side_effect=[False, True, False])
     @patch("viralunity.viralunity_meta.validate_sample_sheet", return_value={})
-    def test_validate_args_krona_db_not_exist(self, mock_validate_sample_sheet, mock_isdir, mock_isfile):
+    def test_validate_args_krona_db_not_exist(
+        self, mock_validate_sample_sheet, mock_isdir, mock_isfile
+    ):
         with self.assertRaises(SystemExit):
             validate_args(self.args)
 
     @patch("os.path.isfile", side_effect=[True, False, False])
     @patch("os.path.isdir", side_effect=[False, True, True])
     @patch("viralunity.viralunity_meta.validate_sample_sheet", return_value={})
-    def test_validate_args_adapters_not_exist(self, mock_validate_sample_sheet, mock_isdir, mock_isfile):
+    def test_validate_args_adapters_not_exist(
+        self, mock_validate_sample_sheet, mock_isdir, mock_isfile
+    ):
         with self.assertRaises(SystemExit):
             validate_args(self.args)
+
+    @patch("os.path.isfile", side_effect=[True, True])
+    @patch("viralunity.viralunity_meta.validate_sample_sheet", return_value={})
+    def test_validate_args_config_file_exists(
+        self, mock_validate_sample_sheet, mock_isfile
+    ):
+        with self.assertRaises(SystemExit):
+            validate_args(self.args)
+
+    @patch("os.path.isfile", side_effect=[True, False])
+    @patch("os.path.isdir", side_effect=[True])
+    @patch("viralunity.viralunity_meta.validate_sample_sheet", return_value={})
+    def test_validate_args_output_dir_exists(
+        self, mock_validate_sample_sheet, mock_isdir, mock_isfile
+    ):
+        with self.assertRaises(SystemExit):
+            validate_args(self.args)
+
+
+class Test_GenerateConfigFile(unittest.TestCase):
+    def setUp(self):
+        self.args = {
+            "sample_sheet": "sample_sheet.csv",
+            "config_file": "config_file.yaml",
+            "output": "output_dir",
+            "run_name": "run_name",
+            "kraken2_database": "kraken2_db",
+            "krona_database": "krona_db",
+            "remove_human_reads": True,
+            "remove_unclassified_reads": False,
+            "adapters": "adapters.fasta",
+            "minimum_read_length": 50,
+            "trim": 0,
+            "create_config_only": False,
+            "threads": 1,
+            "threads_total": 1,
+        }
+
+    @patch("builtins.open", new_callable=unittest.mock.mock_open)
+    @patch("viralunity.viralunity_meta.define_job_id", return_value="job_id")
+    def test_generate_config_file_illumina(self, mock_define_job_id, mock_open):
+        self.args["data_type"] = "illumina"
+        self.samples = {
+            "sample1": ["sample1_R1.fastq", "sample1_R2.fastq"],
+            "sample2": ["sample2_R1.fastq", "sample2_R2.fastq"],
+        }
+        generate_config_file(self.samples, self.args)
+        mock_open.assert_called_once_with("config_file.yaml", "w")
+        handle = mock_open()
+        handle.write.assert_any_call("samples:\n")
+        handle.write.assert_any_call(
+            "    sample-sample1: sample1_R1.fastq sample1_R2.fastq\n"
+        )
+        handle.write.assert_any_call(
+            "    sample-sample2: sample2_R1.fastq sample2_R2.fastq\n"
+        )
+        handle.write.assert_any_call("data: illumina\n")
+        handle.write.assert_any_call("kraken2_database: kraken2_db\n")
+        handle.write.assert_any_call("krona_database: krona_db\n")
+        handle.write.assert_any_call("threads: 1\n")
+        handle.write.assert_any_call("output: output_dir/job_id/\n")
+        handle.write.assert_any_call("adapters: adapters.fasta\n")
+        handle.write.assert_any_call("minimum_length: 50\n")
+        handle.write.assert_any_call("trim: 0\n")
+        handle.write.assert_any_call("remove_human_reads: True\n")
+        handle.write.assert_any_call("remove_unclassified_reads: False\n")
+
+    @patch("builtins.open", new_callable=unittest.mock.mock_open)
+    @patch("viralunity.viralunity_meta.define_job_id", return_value="job_id")
+    def test_generate_config_file_nanopore(self, mock_define_job_id, mock_open):
+        self.args["data_type"] = "nanopore"
+        self.samples = {
+            "sample1": ["sample1.fastq"],
+            "sample2": ["sample2.fastq"],
+        }
+        generate_config_file(self.samples, self.args)
+        mock_open.assert_called_once_with("config_file.yaml", "w")
+        handle = mock_open()
+        handle.write.assert_any_call("samples:\n")
+        handle.write.assert_any_call("    sample-sample1: sample1.fastq\n")
+        handle.write.assert_any_call("    sample-sample2: sample2.fastq\n")
+        handle.write.assert_any_call("data: nanopore\n")
+        handle.write.assert_any_call("kraken2_database: kraken2_db\n")
+        handle.write.assert_any_call("krona_database: krona_db\n")
+        handle.write.assert_any_call("threads: 1\n")
+        handle.write.assert_any_call("output: output_dir/job_id/\n")
+        handle.write.assert_any_call("remove_human_reads: True\n")
+        handle.write.assert_any_call("remove_unclassified_reads: False\n")
+
+
+class Test_MainFunction(unittest.TestCase):
+    @patch(
+        "viralunity.viralunity_meta.get_args",
+        return_value={
+            "config_file": "config_file.yaml",
+            "threads_total": 1,
+            "data_type": "illumina",
+            "create_config_only": False,
+        },
+    )
+    @patch("viralunity.viralunity_meta.validate_args", return_value={})
+    @patch("viralunity.viralunity_meta.generate_config_file")
+    @patch("viralunity.viralunity_meta.snakemake", return_value=True)
+    def test_main_success(
+        self,
+        mock_snakemake,
+        mock_generate_config_file,
+        mock_validate_args,
+        mock_get_args,
+    ):
+        result = main()
+        self.assertEqual(result, 0)
+        mock_snakemake.assert_called_once()
+
+    @patch(
+        "viralunity.viralunity_meta.get_args",
+        return_value={
+            "config_file": "config_file.yaml",
+            "threads_total": 1,
+            "data_type": "illumina",
+            "create_config_only": True,
+        },
+    )
+    @patch("viralunity.viralunity_meta.validate_args", return_value={})
+    @patch("viralunity.viralunity_meta.generate_config_file")
+    @patch("viralunity.viralunity_meta.snakemake", return_value=True)
+    def test_main_create_config_only(
+        self,
+        mock_snakemake,
+        mock_generate_config_file,
+        mock_validate_args,
+        mock_get_args,
+    ):
+        result = main()
+        self.assertEqual(result, 0)
+        mock_snakemake.assert_not_called()
+
+    @patch(
+        "viralunity.viralunity_meta.get_args",
+        return_value={
+            "config_file": "config_file.yaml",
+            "threads_total": 1,
+            "data_type": "illumina",
+            "create_config_only": False,
+        },
+    )
+    @patch("viralunity.viralunity_meta.validate_args", return_value={})
+    @patch("viralunity.viralunity_meta.generate_config_file")
+    @patch("viralunity.viralunity_meta.snakemake", return_value=False)
+    def test_main_failure(
+        self,
+        mock_snakemake,
+        mock_generate_config_file,
+        mock_validate_args,
+        mock_get_args,
+    ):
+        result = main()
+        self.assertEqual(result, 1)
+        mock_snakemake.assert_called_once()
 
 
 if __name__ == "__main__":
--- viralunity/viralunity_meta.py
+++ viralunity/viralunity_meta.py
@@ -12,106 +12,156 @@ import argparse
 from snakemake import snakemake
 from viralunity.viralunity_consensus import validate_sample_sheet, define_job_id
 
+
 def get_args(args):
-    
+
     parser = argparse.ArgumentParser(
-    description='A script to generate config files and run the viralunity metagenomics pipeline',
-    usage='''viralunity_meta.py [args]''')
+        description="A script to generate config files and run the viralunity metagenomics pipeline",
+        usage="""viralunity_meta.py [args]""",
+    )
 
-    parser.add_argument('--data-type', type = str,
-    help='Sequencing data type (illumina or nanopore).',
-    choices = ['illumina','nanopore'],
-    nargs='?',required = True)
-    
-    parser.add_argument('--sample-sheet',
-    help='Complete path for a csv file with samples data paths and metadata',
-    required = True)
- 
-    parser.add_argument('--config-file', 
-    help='Complete path for input (viralunity config) file to be created.',
-    required = True)
-    
-    parser.add_argument('--output', 
-    help='Complete path for output directory to be created by viralunity.',
-    required = True)
+    parser.add_argument(
+        "--data-type",
+        type=str,
+        help="Sequencing data type (illumina or nanopore).",
+        choices=["illumina", "nanopore"],
+        nargs="?",
+        required=True,
+    )
 
-    parser.add_argument('--run-name', type = str, 
-    help='Name for the sequencing run (optional).',
-    nargs='?',const=1, default='undefined')
-    
-    parser.add_argument('--kraken2-database', 
-    help='Complete path for the kraken2 database directory',
-    required = True)
+    parser.add_argument(
+        "--sample-sheet",
+        help="Complete path for a csv file with samples data paths and metadata",
+        required=True,
+    )
 
-    parser.add_argument('--krona-database', 
-    help='Complete path for the krona taxonomic database',
-    required = True)
+    parser.add_argument(
+        "--config-file",
+        help="Complete path for input (viralunity config) file to be created.",
+        required=True,
+    )
 
-    parser.add_argument("--remove-human-reads",
-    help="Remove human reads from krona plot (boolean)",
-    action='store_true')
+    parser.add_argument(
+        "--output",
+        help="Complete path for output directory to be created by viralunity.",
+        required=True,
+    )
 
-    parser.add_argument("--remove-unclassified-reads",
-    help="Remove unclassified reads from krona plot (boolean)",
-    action='store_true')
+    parser.add_argument(
+        "--run-name",
+        type=str,
+        help="Name for the sequencing run (optional).",
+        nargs="?",
+        const=1,
+        default="undefined",
+    )
 
-    parser.add_argument('--adapters', 
-    help='Complete path for adapters sequences in fasta format [Illumina QC]')
+    parser.add_argument(
+        "--kraken2-database",
+        help="Complete path for the kraken2 database directory",
+        required=True,
+    )
 
-    parser.add_argument('--minimum-read-length',type = int,
-    help='Minimum read length threshold (Default = 50) [Illumina QC]',
-    nargs='?',const=1, default=50)
+    parser.add_argument(
+        "--krona-database",
+        help="Complete path for the krona taxonomic database",
+        required=True,
+    )
 
-    parser.add_argument('--trim',type = int,
-    help="Number of bases to trim from the 5' end of reads (Default = 0) [Illumina QC]",
-    nargs='?',const=1, default=0)
+    parser.add_argument(
+        "--remove-human-reads",
+        help="Remove human reads from krona plot (boolean)",
+        action="store_true",
+    )
 
-    parser.add_argument("--create-config-only",
-    help="Only create config file, not running the workflow (boolean)",
-    action='store_true')
+    parser.add_argument(
+        "--remove-unclassified-reads",
+        help="Remove unclassified reads from krona plot (boolean)",
+        action="store_true",
+    )
 
-    parser.add_argument('--threads',type = int,
-    help='Number of available threads for individual tasks (Default = 1)',
-    nargs='?',const=1, default=1)
+    parser.add_argument(
+        "--adapters",
+        help="Complete path for adapters sequences in fasta format [Illumina QC]",
+    )
 
-    parser.add_argument('--threads-total',type = int,
-    help='Number of available threads for the entire workflow (Default = 1)',
-    nargs='?',const=1, default=1)
+    parser.add_argument(
+        "--minimum-read-length",
+        type=int,
+        help="Minimum read length threshold (Default = 50) [Illumina QC]",
+        nargs="?",
+        const=1,
+        default=50,
+    )
+
+    parser.add_argument(
+        "--trim",
+        type=int,
+        help="Number of bases to trim from the 5' end of reads (Default = 0) [Illumina QC]",
+        nargs="?",
+        const=1,
+        default=0,
+    )
+
+    parser.add_argument(
+        "--create-config-only",
+        help="Only create config file, not running the workflow (boolean)",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--threads",
+        type=int,
+        help="Number of available threads for individual tasks (Default = 1)",
+        nargs="?",
+        const=1,
+        default=1,
+    )
+
+    parser.add_argument(
+        "--threads-total",
+        type=int,
+        help="Number of available threads for the entire workflow (Default = 1)",
+        nargs="?",
+        const=1,
+        default=1,
+    )
 
     args = vars(parser.parse_args(args))
     return args
 
+
 def validate_args(args):
-    
-    if os.path.isfile(args['sample_sheet']):
-        sample_sheet = args['sample_sheet']
-        samples = validate_sample_sheet(sample_sheet,args)
+
+    if os.path.isfile(args["sample_sheet"]):
+        sample_sheet = args["sample_sheet"]
+        samples = validate_sample_sheet(sample_sheet, args)
     else:
         print("Sample sheet file does not exist, please verify.")
         exit()
-    
-    if(os.path.isfile(args['config_file'])):
+
+    if os.path.isfile(args["config_file"]):
         print("Config file already exists. Please specify a new one.")
         exit()
 
-    if(os.path.isdir(args['output'])):
+    if os.path.isdir(args["output"]):
         print("Output directory already exists. Please specify a new one.")
         exit()
 
-    if(os.path.isdir(args['kraken2_database'])):
+    if os.path.isdir(args["kraken2_database"]):
         print("kraken2 database directory exists.")
     else:
         print("kraken2 database directory does not exist, please verify.")
         exit()
 
-    if(os.path.isdir(args['krona_database'])):
+    if os.path.isdir(args["krona_database"]):
         print("krona taxonomic database directory exists.")
     else:
         print("krona taxonomic database does not exist, please verify.")
         exit()
 
-    if args['data_type'] == 'illumina':
-        if(os.path.isfile(args['adapters'])):
+    if args["data_type"] == "illumina":
+        if os.path.isfile(args["adapters"]):
             print("Illumina adapter sequences file exists.")
         else:
             print("Illumina adapter sequences file not found, please verify.")
@@ -119,86 +169,87 @@ def validate_args(args):
 
     print("All arguments were succesfully verified.")
 
-    return(samples)
+    return samples
 
-def generate_config_file(samples,args):
-    with open(args['config_file'], 'w') as f:
 
-        samples_string = 'samples:' + "\n"
+def generate_config_file(samples, args):
+    data_type = args["data_type"]
+    with open(args["config_file"], "w") as f:
+
+        samples_string = "samples:\n"
         f.write(samples_string)
+        # TODO: refactor for clarity
         for sample in samples.keys():
-            if args['data_type'] == 'illumina':
-                my_sample = "    sample-" + str(sample) + ": " + samples[sample][0] + " " + samples[sample][1] + "\n"
+            if data_type == "illumina":
+                my_sample = (
+                    f"    sample-{sample}: {samples[sample][0]} {samples[sample][1]}\n"
+                )
             else:
-                my_sample = "    sample-" + str(sample) + ": " + samples[sample][0] + "\n"
+                my_sample = f"    sample-{sample}: {samples[sample][0]}\n"
             f.write(my_sample)
 
-        data = "data: " + args['data_type'] + "\n"
-        f.write(data)
-        
-        kraken2_database = "kraken2_database: " + args['kraken2_database'] + "\n"
-        f.write(kraken2_database)
+        data_line = f"data: {data_type}\n"
+        f.write(data_line)
 
-        krona_database = "krona_database: " + args['krona_database'] + "\n"
-        f.write(krona_database)
-        
-        threads = "threads: " + str(args['threads']) + "\n"
-        f.write(threads)
+        kraken2_database_line = f"kraken2_database: {args['kraken2_database']}\n"
+        f.write(kraken2_database_line)
 
-        job_id = define_job_id(args)
-        output = "output: " + args['output'] + "/" + job_id + "/" + "\n"
-        f.write(output)
+        krona_database_line = f"krona_database: {args['krona_database']}\n"
+        f.write(krona_database_line)
 
-        if args['data_type'] == 'illumina':
-            adapters = "adapters: " + args['adapters'] + "\n"
-            f.write(adapters)
+        threads_line = f"threads: {str(args['threads'])}\n"
+        f.write(threads_line)
 
-            minimum_length = "minimum_length: " + str(args['minimum_read_length']) + "\n"
-            f.write(minimum_length)
+        output_line = f"output: {args['output']}/{define_job_id(args)}/\n"
+        f.write(output_line)
 
-            trim = "trim: " + str(args['trim']) + "\n"
-            f.write(trim)
+        # TODO: Analyze if it is possible to make an adapter pattern with illumina/nanopore
+        if data_type == "illumina":
+            adapters_line = f"adapters: {args['adapters']}\n"
+            f.write(adapters_line)
 
-        if args['remove_human_reads']:
-            remove_human_reads = "remove_human_reads: True" + "\n"
-        else: 
-            remove_human_reads = "remove_human_reads: False" + "\n"
-        f.write(remove_human_reads)
+            minimum_length_line = (
+                f"minimum_length: {str(args['minimum_read_length'])}\n"
+            )
+            f.write(minimum_length_line)
 
-        if args['remove_unclassified_reads']:
-            remove_unclassified_reads = "remove_unclassified_reads: True" + "\n"
-        else: 
-            remove_unclassified_reads = "remove_unclassified_reads: False" + "\n"
-        f.write(remove_unclassified_reads)
+            trim_line = f"trim: {str(args['trim'])}\n"
+            f.write(trim_line)
 
-        print("The config file was generated. -> ", args['config_file'])
+        remove_human_reads_line = f"remove_human_reads: {args['remove_human_reads']}\n"
+        f.write(remove_human_reads_line)
+
+        remove_unclassified_reads_line = (
+            f"remove_unclassified_reads: {args['remove_unclassified_reads']}\n"
+        )
+        f.write(remove_unclassified_reads_line)
+
+        print("The config file was generated. -> ", args["config_file"])
 
     return
+
 
 def main():
     args = get_args(sys.argv[1:])
     samples = validate_args(args)
-    generate_config_file(samples,args)
+    generate_config_file(samples, args)
 
-    config = args['config_file']
-    cores = args['threads_total']
+    config = args["config_file"]
+    cores = args["threads_total"]
     target_rule = "all"
 
-    if args['data_type'] == 'illumina':
-        workflow_path = sys.path[0] + '/metagenomics_illumina.smk'
-    else:
-        workflow_path = sys.path[0] + '/metagenomics_nanopore.smk'
+    workflow_path = sys.path[0] + f"/metagenomics_{args['data_type']}.smk"
 
-    if args['create_config_only']:
-        print('Finished.')
-        status = True
-    else:
-        status = snakemake(workflow_path, configfiles=[config], cores=cores,targets=[target_rule])
-
-    if status:
+    if args["create_config_only"]:
+        print("Finished.")
         return 0
-    else:
-        return 1
 
-if __name__  == '__main__':
+    successful = snakemake(
+        workflow_path, configfiles=[config], cores=cores, targets=[target_rule]
+    )
+
+    return 0 if successful else 1
+
+
+if __name__ == "__main__":
     sys.exit(main())
